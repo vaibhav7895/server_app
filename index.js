90c0e139212a5c3a4e55230cd4f0527ab4d46507
Vaibhav <vaibhav7895@gmail.com>
2024-06-24 13:30:13 +0530

    chat

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const io = require('socket.io')(8080, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://chat-jhmy6e8w3-vaibhav7895s-projects.vercel.app/',
     }
 });
 
